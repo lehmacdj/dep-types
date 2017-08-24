e55ed04ea53b16ad0cbf1467c1a92c5c85690e13
Devin Lehmacher <lehmacdj@gmail.com>
2017-08-24 17:25:16 -0400

    implement type-checker, evaluator

--- src/Lang/Dependent/AST.hs
+++ src/Lang/Dependent/AST.hs
@@ -6,6 +6,8 @@ module Lang.Dependent.AST where
 
 import Numeric.Natural
 
+import Control.Monad.Reader
+
 import Control.Lens
 import Control.Lens.Plated
 import Data.Map (Map)
@@ -69,7 +71,7 @@ instance Substitutable Name Term Term where
         where rep' = incrVar x rep
     substitute x rep t = over plate (substitute x rep) t
 
-type Env = Map (Name, Int) Term
+type Env = Map Name Term
 
 mapVarIndex :: (Int -> Int) -> Name -> Term -> Term
 mapVarIndex f x = transform go
@@ -78,15 +80,84 @@ mapVarIndex f x = transform go
             | otherwise = V y i
           go t = t
 
-getFromEnv :: Env -> (Name, Int) -> Either String Term
-getFromEnv env a = maybe (Left err) Right (M.lookup a env) where
-    err = show a ++ " is not present in the type environment"
+getFromEnv :: Env -> Name -> Either String Term
+getFromEnv env x = maybe (Left err) Right (M.lookup x env) where
+    err = show x ++ " is not present in the type environment"
 
-typeOf :: Term -> Either String Term
-typeOf = undefined
+extendedWith :: (Name, Term) -> Env -> Env
+extendedWith (k, v) = M.insert k v
+
+-- assert that two terms are equal, for example: during type checking
+assertEq :: Term -> Term -> Either String ()
+assertEq s t
+  | s == t = pure ()
+  | otherwise = Left $ "term " ++ show s ++ " is not equal to " ++ show t
+
+-- (f : (πx:Type.x)) Bool
+
+substitute' :: Name -> Term -> Term -> Term
+substitute' = ((nf.).) . substitute
+
+typeOf' :: Env -> Term -> Either String Term
+typeOf' = (fmap nf .) .  typeOf
+
+typeOf :: Env -> Term -> Either String Term
+typeOf g Unit = Right UnitTy
+typeOf g T = Right Bool
+typeOf g F = Right Bool
+typeOf g Void = Right $ TypeUniverse 0
+typeOf g UnitTy = Right $ TypeUniverse 0
+typeOf g Bool = Right $ TypeUniverse 0
+typeOf g (TypeUniverse n) = Right $ TypeUniverse $ n + 1
+typeOf g (IF p t e) = do
+    p' <- typeOf' g p
+    t' <- typeOf' g t
+    e' <- typeOf' g e
+    assertEq Bool p'
+    assertEq t' e'
+    pure t'
+typeOf g (App s t) = do
+    s' <- typeOf' g s
+    t' <- typeOf' g t
+    case s' of
+        Pi x x' r' -> do
+            assertEq x' t'
+            Right $ substitute' x t r'
+        _ -> Left $ "type of " ++ show t ++ " was "
+                 ++ show t' ++ " but was expected to be a function type"
+typeOf g (Pi x x' e) = do
+    e' <- typeOf' (extendedWith (x, x') g) e
+    case (x', e') of
+        -- this may not be particualarly sound. See this for more details:
+        -- https://cs.stackexchange.com/questions/13285/universes-in-dependent-type-theory
+        (TypeUniverse i, TypeUniverse j) -> Right $ TypeUniverse $ max i j
+        _ -> Left $ "function arguments and return types must be types"
+                 ++ "and cannot be values"
+typeOf g (Lam x x' e) = do
+    e' <- typeOf' (extendedWith (x, x') g) e
+    Right $ Pi x x' e'
+typeOf g (V x 0) = getFromEnv g x
 
 nf :: Term -> Term
-nf = undefined
+nf Unit = Unit
+nf T = T
+nf F = F
+nf Void = Void
+nf UnitTy = UnitTy
+nf Bool = Bool
+nf (TypeUniverse n) = TypeUniverse n
+nf (IF p t e) = case nf p of
+    T -> nf t
+    F -> nf e
+nf (App f a) = case whnf f of
+    Lam x x' e -> nf $ substitute x a e
+    _ -> error "invalid function application"
+nf (Lam x x' e) = Lam x (nf x') (nf e)
+nf (Pi x x' e) = Pi x (nf x') (nf e)
+nf (V x i) = V x i
 
 whnf :: Term -> Term
-whnf = undefined
+whnf (App f a) = case whnf f of
+    Lam x x' e -> whnf $ substitute x a e
+    _ -> error "invalid function application"
+whnf t = t
