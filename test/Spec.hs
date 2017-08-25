7c63b10f5464c684beb0b3331e31c7866d0f7203
Devin Lehmacher <lehmacdj@gmail.com>
2017-08-25 15:15:16 -0400

    add a new property

--- test/Spec.hs
+++ test/Spec.hs
@@ -17,9 +17,13 @@ properties = testGroup "QuickCheck property tests"
     [ testProperty "substitution" $
         \(x :: Name) (t :: Term) ->
             x `notElem` freeVars t ==> substitute x t t == t
-    , testProperty "well typed term doesn't fail reduction" $
+    , testProperty "progress" $
         \(t :: Term) ->
             isRight (typeCheck t) ==> isRight (nf t)
+    , testProperty "preservation" $
+        \(t :: Term) ->
+            let t' = typeOf' [] t
+             in isRight t' ==> t' == (nf t >>= typeOf' [])
     ]
 
 instance Arbitrary Term where
@@ -27,18 +31,11 @@ instance Arbitrary Term where
 
 aterm :: Int -> Gen Term
 aterm 0 = oneof
-    [ V <$> arbitrary <*> (mod <$> arbitrary <*> pure 3)
+    [ V <$> aname <*> (mod <$> arbitrary <*> pure 3)
     , pure Unit , pure UnitTy
     , pure T, pure F, pure Bool
     , TypeUniverse <$> arbitrary ]
-
-aterm n = oneof
-    [ liftA3 Lam arbitrary t t
-    , liftA3 Pi arbitrary t t
-    , liftA3 IF t t t
-    , liftA2 App t t
-    ]
-        where t = aterm (n `div` 2)
+aterm n = oneof [ liftA3 Lam aname t t , liftA3 Pi aname t t , liftA3 IF t t t , liftA2 App t t ] where t = aterm (n `div` 2)
 
 newtype WellTyped = WellTyped Term
 
@@ -46,9 +43,5 @@ infixr 8 <.>
 (<.>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
 (<.>) = fmap . fmap
 
-instance Arbitrary WellTyped where
-    arbitrary = sized (WellTyped . fst <.> tyterm)
-
-tyterm :: Int -> Gen (Term, Term)
-tyterm 0 = undefined
-tyterm n = undefined
+aname :: Gen Name
+aname = listOf1 $ choose ('a', 'z')
